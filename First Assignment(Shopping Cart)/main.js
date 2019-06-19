7ccb9fb84dab1ba25129b65c6ab8fc0b549a1278
Delower <delowerhosencuet13@gmail.com>
2019-06-20 04:28:41 +0600

    Design modified

--- First Assignment(Shopping Cart)/main.js
+++ First Assignment(Shopping Cart)/main.js
@@ -2,6 +2,11 @@ let quantity;
 let addedToCart = new Array();
 
 let items = document.getElementsByClassName('btn');
+// document.getElementById('section-header').innerHTML = localStorage.getItem('value');
+document.getElementById('clear').addEventListener('click', ()=>{
+    localStorage.clear();
+    location.reload();
+});
 
 for (let index = 0; index < items.length; index++) {
     let checkCart = localStorage.getItem(index);
@@ -13,7 +18,12 @@ for (let index = 0; index < items.length; index++) {
         if (!checkCart && !addedToCart[index]) {
             quantity++;
             localStorage.setItem('value', quantity);
-            document.getElementById('section-header').innerHTML = localStorage.getItem('value');
+            if(localStorage.getItem('value')>0){
+                document.getElementById('section-header').innerHTML = localStorage.getItem('value');
+            }else{
+                document.getElementById('section-header').innerHTML = 0;
+            }
+            
             localStorage.setItem(index, 1);
             addedToCart[index] = 1;
         }
@@ -23,4 +33,9 @@ for (let index = 0; index < items.length; index++) {
         }
     }
 }
-document.getElementById('section-header').innerHTML = localStorage.getItem('value');
+
+if(localStorage.getItem('value')>0){
+    document.getElementById('section-header').innerHTML = localStorage.getItem('value');
+}else{
+    document.getElementById('section-header').innerHTML = 0;
+}
